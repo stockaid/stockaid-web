1d998671a132c8d90d0d2ce479fd3a7bcc566407
Allan Almazan <allan.almazan@gmail.com>
2019-10-22 21:53:00 -0700

    Update users to have UserProfile instead of saving everything on the User

--- stockaid/users/models.py
+++ stockaid/users/models.py
@@ -1,14 +1,23 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    """User model to contain mostly authentication-related data."""
+    # Removing first and last name and using a general `name` field
+    # in `UserProfile`.
+    first_name = None
+    last_name = None
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class UserProfile(models.Model):
+    """Model to contain all non-authentication data for a user."""
+
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    title = models.CharField("Job title of User", blank=True, max_length=255)
